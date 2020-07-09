614f67eab9c009f1f6583ef975b77bdf7903a6f3
Rachel Kat <rachelkat@gmail.com>
2020-07-08 23:01:26 -0400

    base setup for react and spring boot

---
+++ course-manager-spring-boot/src/main/java/com/app/coursemanager/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.app.coursemanager;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @RequestMapping("/welcome")
+    public String welcome() {
+        return "Welcome!";
+    }
+}
