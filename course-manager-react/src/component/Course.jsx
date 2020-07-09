6b897be6ab7a80ba835e72bc8e25caf731c0759d
Rachel Kat <rachelkat@gmail.com>
2020-07-09 19:46:55 -0400

    add form validation

--- course-manager-react/src/component/Course.jsx
+++ course-manager-react/src/component/Course.jsx
@@ -2,41 +2,65 @@ import React, { Component } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import CourseDataService from '../service/CourseDataService';
 
-const INSTRUCTOR = 'in28minutes'
+const INSTRUCTOR = 'in28minutes';
 
 class Course extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       id: this.props.match.params.id,
       description: ''
-    }
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.validate = this.validate.bind(this);
 
   }
 
   componentDidMount() {
 
-    console.log(this.state.id)
+    console.log(this.state.id);
 
     // eslint-disable-next-line
     if (this.state.id == '-1') {
-      return
+      return;
     }
 
     CourseDataService.retrieveCourse(INSTRUCTOR, this.state.id)
       .then(response => this.setState({
         description: response.data.description
-      }))
+      }));
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  validate(values) {
+    let errors = {};
+    if (!values.description) {
+      errors.description = 'Enter a Description';
+    } else if (values.description.length < 5) {
+      errors.description = 'Enter atleast 5 Characters in Description';
+    }
+
+    return errors;
+
   }
 
   render() {
-    let { description, id } = this.state
+    let { description, id } = this.state;
     return (
       <>
         <h3>Course</h3>
         <div className="container">
-          <Formik initialValues={{ id, description }}>
+          <Formik initialValues={{ id, description }}
+            onSubmit={this.onSubmit}
+            validateOnChange={false}
+            validateOnBlur={false}
+            validate={this.validate}
+            enableReinitialize={true}>
             {
               (props) => (
                 <Form>
